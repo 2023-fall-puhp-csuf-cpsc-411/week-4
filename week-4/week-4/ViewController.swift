5ff9bd1b584e4ef7d8e2b7d68afa958237a7bbbc
Mark Raden <mraden@csu.fullerton.edu>
2023-09-12 16:55:19 -0700

    added functions and actions for the uI

--- week-4/week-4/ViewController.swift
+++ week-4/week-4/ViewController.swift
@@ -15,6 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet var slider: UISlider!
     @IBOutlet var mySwitch: UISwitch!
     @IBOutlet var progress: UIProgressView!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button tapped")
+    }
+    
+    @IBAction func draggedButton(_ sender: UIButton) {
+        print("Dragged a button")
+    }
+    
+    @IBAction func segmentedControlChanges(_ sender: UISegmentedControl) {
+        print("The segmented control was changed")
+    }
+    
+    @IBAction func textEditChanged(_ sender: UITextField) {
+        let text = sender.text!
+        print("The text field was changed to: \(text)")
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        print("the slider has changed to: \(sender.value)")
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        print("the switch is now: \(sender.isOn)")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
