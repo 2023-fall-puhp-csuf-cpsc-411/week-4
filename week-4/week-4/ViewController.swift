47a8a1723dbecf200fe8799084f620173054beed
Mark Raden <mraden@csu.fullerton.edu>
2023-09-12 16:42:47 -0700

    connected the buttons to their viewcontroller

--- week-4/week-4/ViewController.swift
+++ week-4/week-4/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var textEdit: UITextField!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var mySwitch: UISwitch!
+    @IBOutlet var progress: UIProgressView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
